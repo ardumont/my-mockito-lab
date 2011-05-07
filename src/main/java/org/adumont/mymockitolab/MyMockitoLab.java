ac59c5f208786e6b857bfc92bf92eb7fd9b05bc7
dumont.a@sfeir.com <dumont.a@sfeir.com>
2011-05-07 18:32:51 +0200

    Part Verifying exact number of invocations / at least x / never.

--- src/main/java/org/adumont/mymockitolab/MyMockitoLab.java
+++ src/main/java/org/adumont/mymockitolab/MyMockitoLab.java
@@ -3,6 +3,11 @@ package org.adumont.mymockitolab;
 //Let's import Mockito statically so that the code looks clearer
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -90,4 +95,44 @@ public class MyMockitoLab {
 	// }
 	// }
 
+	@SuppressWarnings("unchecked")
+	@Test
+	public void verifyNumberOfInvocations() {
+		// using mock
+		mockedList.add("once");
+
+		mockedList.add("twice");
+		mockedList.add("twice");
+
+		mockedList.add("three times");
+		mockedList.add("three times");
+		mockedList.add("three times");
+
+		mockedList.add("five times");
+		mockedList.add("five times");
+		mockedList.add("five times");
+		mockedList.add("five times");
+		mockedList.add("five times");
+		mockedList.add("five times");
+
+		// following two verifications work exactly the same - times(1) is used
+		// by default
+		verify(mockedList).add("once");
+		verify(mockedList, times(1)).add("once");
+
+		// exact number of invocations verification
+		verify(mockedList, times(2)).add("twice");
+		verify(mockedList, times(3)).add("three times");
+
+		// verification using never(). never() is an alias to times(0)
+		verify(mockedList, times(0)).add("never happened");
+		verify(mockedList, never()).add("never happened");
+
+		// verification using atLeast()/atMost()
+		verify(mockedList, atLeastOnce()).add("three times");
+		verify(mockedList, atMost(5)).add("three times");
+
+		verify(mockedList, atLeast(2)).add("five times");
+		verify(mockedList, atMost(6)).add("five times");
+	}
 }
